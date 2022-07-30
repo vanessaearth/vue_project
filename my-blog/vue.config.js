e8ac9c106ea44e681a310d9f56886641963b77a5
yangjin <yangjin@weimiao.cn>
2022-07-30 08:40:15 +0800

    fix: fix

--- my-blog/vue.config.js
+++ my-blog/vue.config.js
@@ -6,6 +6,20 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+  // publicPath: '/start',
+  // assetsDir: 'static', //设置css,js,img等在static文件中
+  // outputDir: process.env.VUE_APP_OUTPUT,
+  // publicPath: '/demo',
+  // 多入口文件配置
+  // pages: {
+  //   index: {
+  //     entry: 'src/index/main.js',
+  //     template: 'public/index.html',
+  //     filename: 'index.html',
+  //     chunks: ['chunk-vendors', 'chunk-common', 'index']
+  //   },
+  //   second: 'src/second/main.js'
+  // },
   transpileDependencies: true,
   productionSourceMap: false,
   devServer: {
@@ -43,6 +57,18 @@ module.exports = defineConfig({
     }
   },
   chainWebpack (config) {
+    // 设置svgIcon的loader
+    // 1.默认的svg规则排除自定义的svg目录
+    config.module.rule('svg')
+      .exclude.add(path.resolve(__dirname, 'src/icons')).end()
+    // 2.新增icon规则只包含icon目录
+    config.module.rule('icons')
+      .test(/\.svg$/)// 规则
+      .include.add(path.resolve(__dirname, 'src/icons')).end()
+      .use('svg-sprite-loader')// 指定loader
+      .loader('svg-sprite-loader')
+      .options({ symbolId: 'icon-[name]' }).end()
+
     // 关闭预加载，减少带宽压力
     // https://cli.vuejs.org/zh/guide/html-and-static-assets.html#preload
     // https://cli.vuejs.org/zh/guide/html-and-static-assets.html#prefetch
@@ -50,10 +76,13 @@ module.exports = defineConfig({
     config.plugins.delete('prefetch')
   },
   configureWebpack: {
+    name: process.env.NODE_ENV === 'development' ? '测试-欢迎来到我的博客站' : '正式-欢迎来到我的博客站',
+
     plugins: [
       new NodePolyfillPlugin(),
       new webpack.DefinePlugin({
         'process.env': {
+
           VUE_ENV: JSON.stringify(process.env.VUE_ENV)
         }
       }),
